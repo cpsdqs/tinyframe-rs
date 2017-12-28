5d065e04eae58997a5755b147c270f1ba4863b8e
cpsdqs <cpsdqs@gmail.com>
2017-12-28 11:34:14 +0100

    Update

--- implementors/core/convert/trait.Into.js
+++ implementors/core/convert/trait.Into.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
-implementors["num_rational"] = ["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">(</a>T, T<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">)</a>&gt; for <a class=\"struct\" href=\"num_rational/struct.Ratio.html\" title=\"struct num_rational::Ratio\">Ratio</a>&lt;T&gt;",];
 implementors["tiny_frame"] = ["impl&lt;I, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>&gt;&gt; for <a class=\"struct\" href=\"tiny_frame/struct.Msg.html\" title=\"struct tiny_frame::Msg\">Msg</a>&lt;I, T&gt;",];
 
             if (window.register_implementors) {
